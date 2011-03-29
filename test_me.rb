c995e6b92785d1d4a85c4f78d575fb272b072910
Giovanni <potomak84@gmail.com>
2011-03-29 17:55:40 +0200

    add threaded implementation; update smoods_board lib name (following ruby naming)

--- test_me.rb
+++ test_me.rb
@@ -1,4 +1,6 @@
-require File.expand_path("../smoodsboard", __FILE__)
+require 'benchmark'
+require File.expand_path("../smoods_board", __FILE__)
+require File.expand_path("../threaded_smoods_board", __FILE__)
 
 def usage
   puts "Usage: ruby test_me.rb <smooder>"
@@ -9,16 +11,33 @@ if ARGV.length != 1
   exit
 end
 
-result = "\nYour FOLLOWING smoodsboard"
-board = SmoodsBoard.new.following_smoodsboard ARGV[0]
-board.each_pair do |mood, stuff|
-  result << "\n#{mood.upcase}: #{stuff[:smooder].username} (#{stuff[:points]})"
-end
+result = ""
+bm_result = ""
 
-result << "\nYour FOLLOWERS smoodsboard"
-board = SmoodsBoard.new.followers_smoodsboard ARGV[0]
-board.each_pair do |mood, stuff|
-  result << "#{mood.upcase}: #{stuff[:smooder].username} (#{stuff[:points]})"
-end
+bm_result << "\n\nthreaded: " + Benchmark.measure {
+  result << "\n\nYour FOLLOWING smoodsboard"
+  board = ThreadedSmoodsBoard.new.following_smoodsboard ARGV[0]
+  board.each_pair do |mood, stuff|
+    result << "\n#{mood.upcase}: #{stuff[:smooder].username} (#{stuff[:points]})"
+  end
+}.format("%r")
+
+bm_result << "\nnormal: " + Benchmark.measure {
+  result << "\n\nYour FOLLOWING smoodsboard"
+  board = SmoodsBoard.new.following_smoodsboard ARGV[0]
+  board.each_pair do |mood, stuff|
+    result << "\n#{mood.upcase}: #{stuff[:smooder].username} (#{stuff[:points]})"
+  end
+}.format("%r")
+
+# puts Benchmark.measure {
+#   result << "\n\nYour FOLLOWERS smoodsboard"
+#   board = SmoodsBoard.new.followers_smoodsboard ARGV[0]
+#   board.each_pair do |mood, stuff|
+#     result << "\n#{mood.upcase}: #{stuff[:smooder].username} (#{stuff[:points]})"
+#   end
+# }
 
 puts result
+
+puts bm_result
